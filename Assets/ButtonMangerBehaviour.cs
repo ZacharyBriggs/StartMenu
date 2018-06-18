f9f2916f0765cec3930ac99c3112fac31d52d642
Zachary Briggs <zacharybriggs64@gmail.com>
2018-06-18 17:49:26 -0500

    initial

---
+++ Assets/ButtonMangerBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "MyStuff")]
+public class GlobalGameManager : ScriptableObject
+{
+    public void StartButton()
+    {
+        Debug.Log("game has started");
+    }
+
+    public void EndButton()
+    {
+        Debug.Log("game has ended");
+    }
+
+    public void OptionsButton()
+    {
+        Debug.Log("options menu opened");
+    }
+}
